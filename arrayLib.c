fa06eb767f78ad54287fc4672e6809f75910b0e5
ertewi <nikita28984@gmail.com>
2023-02-25 22:57:03 +0700

    Input and output arrays functions

---
+++ arrayLib.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void array_input(int *A, int n) {
+
+	printf("Enter array of %d numbers:\n", n);
+	for(int i = 0; i < n; i++) {
+
+		scanf("%d", &A[i]);
+	}
+}
+
+void array_print(int *A, int n) {
+
+	for(int i = 0; i < n; i++) {
+
+		printf(" %d", A[i]);
+	}
+}
